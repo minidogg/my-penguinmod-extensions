cc40ae8318423d9e66146ad557b8d2337c3b478e
AshimeeAlt <161080149+AshimeeAlt@users.noreply.github.com>
2024-03-22 19:16:10 -0400

    fix bf ext

--- brainfuck/index.js
+++ brainfuck/index.js
@@ -1,124 +1,131 @@
-function bf(code, data, input) {
-    let result = ""
-    let memory = data.memory
-    let loc = data.memoryPointer
+(function (Scratch) {
+  function bf(code, data, input) {
+    let result = '';
+    let memory = data.memory;
+    let loc = data.memoryPointer;
     let throwErr = (err, i) => {
-      return ["Error: " + err + ` (Char:${i}, MemoryLocation:${loc})`, {
-        memoryPointer: loc,
-        memory: memory
-      }]
-    }
+      return [
+        'Error: ' + err + ` (Char:${i}, MemoryLocation:${loc})`,
+        {
+          memoryPointer: loc,
+          memory: memory,
+        },
+      ];
+    };
     for (let i = 0; i < code.length; i++) {
       switch (code[i]) {
-        case ("+"):
-          memory[loc]++
+        case '+':
+          memory[loc]++;
           break;
-        case ("-"):
-          memory[loc]--
+        case '-':
+          memory[loc]--;
           break;
-        case (">"):
-          loc++
-          if (memory[loc] == undefined) memory.push(0)
+        case '>':
+          loc++;
+          if (memory[loc] == undefined) memory.push(0);
           break;
-        case ("<"):
-          loc--
+        case '<':
+          loc--;
           if (memory[loc] == undefined) {
-            loc++
-            return throwErr(`Memory location can't go negative!`, i)
+            loc++;
+            return throwErr(`Memory location can't go negative!`, i);
           }
           break;
-        case ("["):
+        case '[':
           if (memory[loc] == 0) {
             while (true) {
-              if (code.length < i || code[i] == "]") break
-              i++
+              if (code.length < i || code[i] == ']') break;
+              i++;
             }
           }
           break;
-        case ("]"):
+        case ']':
           if (memory[loc] != 0) {
             while (true) {
-              if (code.length < i || code[i] == "[") break
-              i--
+              if (code.length < i || code[i] == '[') break;
+              i--;
             }
           }
           break;
-        case ("."):
-          result += String.fromCharCode(memory[loc])
+        case '.':
+          result += String.fromCharCode(memory[loc]);
           break;
-        case (" "):
+        case ' ':
           break;
-        case (","):
-          memory[loc] = input.charCodeAt(0)
+        case ',':
+          memory[loc] = input.charCodeAt(0);
           break;
         default:
-          return throwErr(`Invalid command: ${code[i]}`, i)
+          return throwErr(`Invalid command: ${code[i]}`, i);
       }
     }
-    return [result, {
-      memoryPointer: loc,
-      memory: memory
-    }]
+    return [
+      result,
+      {
+        memoryPointer: loc,
+        memory: memory,
+      },
+    ];
   }
 
-//now the extension
-class BrainFuckInterperter{
-    constructor(){
-        this.contexts = {
-            /*
+  //now the extension
+  class BrainFuckInterperter {
+    constructor() {
+      this.contexts = {
+        /*
                 main:{
                     memoryPointer:0,
                     memory:[0]
                 }
             */
-        }
-        this.allowErrors = false
+      };
+      this.allowErrors = false;
     }
     getInfo() {
       return {
-        id: 'brainfuckinterperter',
+        id: 'minidoggBrainfuckinterperter',
         name: 'BrainFuck Intepreter',
-        color1:"#c200d4",
-        color2:"#a802b8",
-        color3:"#56005e",
+        color1: '#c200d4',
+        color2: '#a802b8',
+        color3: '#56005e',
 
         blocks: [
-            {
-                blockType: Scratch.BlockType.LABEL,
-                text: "Context Management"
-              },
+          {
+            blockType: Scratch.BlockType.LABEL,
+            text: 'Context Management',
+          },
           {
             opcode: 'createContext',
             blockType: Scratch.BlockType.COMMAND,
             text: 'Create/Reset context named: [name]',
             arguments: {
-                name: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:"main"
-                }
-              }
+              name: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'main',
+              },
+            },
           },
           {
             opcode: 'getContext',
             blockType: Scratch.BlockType.REPORTER,
             text: `Get data from context named: [name]`,
             arguments: {
-                name: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:"main"
-                }
-              }
+              name: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'main',
+              },
+            },
           },
           {
             opcode: 'deleteContext',
             blockType: Scratch.BlockType.COMMAND,
             text: 'Delete context named: [name]',
             arguments: {
-                name: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:"main"
-                }
-              }
+              name: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'main',
+              },
+            },
           },
           {
             opcode: 'deleteAllContext',
@@ -128,45 +135,45 @@ class BrainFuckInterperter{
 
           {
             blockType: Scratch.BlockType.LABEL,
-            text: "Code Execution"
+            text: 'Code Execution',
           },
           {
             opcode: 'runCodeReporter',
             blockType: Scratch.BlockType.REPORTER,
             text: 'In context: [name] run: [code] with input byte: [input]',
             arguments: {
-                name: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:"main"
-                },
-                code: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue:">>+"
-                },
-                input: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue:"h"
-                }
-              }
+              name: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'main',
+              },
+              code: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '>>+',
+              },
+              input: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'h',
+              },
+            },
           },
           {
             opcode: 'runCodeCommand',
             blockType: Scratch.BlockType.COMMAND,
             text: 'In context: [name] run: [code] with input byte: [input]',
             arguments: {
-                name: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:"main"
-                },
-                code: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue:">>+"
-                },
-                input: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue:"h"
-                }
-              }
+              name: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'main',
+              },
+              code: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '>>+',
+              },
+              input: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'h',
+              },
+            },
           },
 
           {
@@ -174,17 +181,16 @@ class BrainFuckInterperter{
             blockType: Scratch.BlockType.COMMAND,
             text: 'Set allow errors: [value]',
             arguments: {
-                value:{
-                    type:Scratch.ArgumentType.BOOLEAN,
-                    defaultValue:true
-                }
-                // value: {
-                //     type: Scratch.ArgumentType.STRING,
-                //     menu: 'BOOL_MENU'
-                //   },
-              }
+              value: {
+                type: Scratch.ArgumentType.BOOLEAN,
+                defaultValue: true,
+              },
+              // value: {
+              //     type: Scratch.ArgumentType.STRING,
+              //     menu: 'BOOL_MENU'
+              //   },
+            },
           },
-
         ],
         // menus: {
         //     BOOL_MENU: {
@@ -203,51 +209,49 @@ class BrainFuckInterperter{
         // }
       };
     }
-  
-    allowErrors(args){
-        this.allowErrors = args.value
+
+    allowErrors(args) {
+      args.value = Scratch.Cast.toBoolean(args.value);
+      this.allowErrors = args.value;
     }
 
-    runCodeReporter(args){
-        let result = bf(args.code,this.contexts[args.name],args.input)
-        if(result[0].startsWith("Error:")&&this.allowErrors == false){
-            console.warn(result)
-        }else{
-            this.contexts[args.name] = result[1]
-        }
-        return result[0]
+    runCodeReporter(args) {
+      args.code = Scratch.Cast.toString(args.code);
+      args.name = Scratch.Cast.toString(args.name);
+      args.input = Scratch.Cast.toString(args.input);
+      let result = bf(args.code, this.contexts[args.name], args.input);
+      if (result[0].startsWith('Error:') && this.allowErrors == false) {
+        console.warn(result);
+      } else {
+        this.contexts[args.name] = result[1];
+      }
+      return result[0];
     }
-    runCodeCommand(args){
-        let result = bf(args.code,this.contexts[args.name],args.input)
-        if(result[0].startsWith("Error:")&&this.allowErrors == false){
-            return result[0]
-        }else{
-            this.contexts[args.name] = result[1]
-        }
-        // return result[0]
+    runCodeCommand(args) {
+      // reduce boiler plate by just calling the preexisting function
+      this.runCodeReporter(args);
     }
-    
 
     createContext(args) {
-        this.contexts[args.name] = {
-            memoryPointer:0,
-            memory:[0]
-        }
+      args.name = Scratch.Cast.toString(args.name);
+      this.contexts[args.name] = {
+        memoryPointer: 0,
+        memory: [0],
+      };
     }
-    getContext(args){
-        return JSON.stringify(this.contexts[args.name])
+    getContext(args) {
+      args.name = Scratch.Cast.toString(args.name);
+      return JSON.stringify(this.contexts[args.name]);
     }
 
     deleteContext(args) {
-        delete this.contexts[args.name]
+      args.name = Scratch.Cast.toString(args.name);
+      delete this.contexts[args.name];
     }
     deleteAllContext(args) {
-        this.contexts = {}
+      this.contexts = {};
     }
-
-   
-
-
   }
-  
+
   Scratch.extensions.register(new BrainFuckInterperter());
+})(Scratch);

22d7c7de85c3fd69a84c9caa6dde40fde2255ccf
UnluckyCrafter <117037081+minidogg@users.noreply.github.com>
2024-03-22 08:30:46 -0500

    scuffed moment

--- brainfuck/index.js
+++ brainfuck/index.js
@@ -1,3 +1,67 @@
+function bf(code, data, input) {
+    let result = ""
+    let memory = data.memory
+    let loc = data.memoryPointer
+    let throwErr = (err, i) => {
+      return ["Error: " + err + ` (Char:${i}, MemoryLocation:${loc})`, {
+        memoryPointer: loc,
+        memory: memory
+      }]
+    }
+    for (let i = 0; i < code.length; i++) {
+      switch (code[i]) {
+        case ("+"):
+          memory[loc]++
+          break;
+        case ("-"):
+          memory[loc]--
+          break;
+        case (">"):
+          loc++
+          if (memory[loc] == undefined) memory.push(0)
+          break;
+        case ("<"):
+          loc--
+          if (memory[loc] == undefined) {
+            loc++
+            return throwErr(`Memory location can't go negative!`, i)
+          }
+          break;
+        case ("["):
+          if (memory[loc] == 0) {
+            while (true) {
+              if (code.length < i || code[i] == "]") break
+              i++
+            }
+          }
+          break;
+        case ("]"):
+          if (memory[loc] != 0) {
+            while (true) {
+              if (code.length < i || code[i] == "[") break
+              i--
+            }
+          }
+          break;
+        case ("."):
+          result += String.fromCharCode(memory[loc])
+          break;
+        case (" "):
+          break;
+        case (","):
+          memory[loc] = input.charCodeAt(0)
+          break;
+        default:
+          return throwErr(`Invalid command: ${code[i]}`, i)
+      }
+    }
+    return [result, {
+      memoryPointer: loc,
+      memory: memory
+    }]
+  }
+
+//now the extension
 class BrainFuckInterperter{
     constructor(){
         this.contexts = {
@@ -8,11 +72,16 @@ class BrainFuckInterperter{
                 }
             */
         }
+        this.allowErrors = false
     }
     getInfo() {
       return {
         id: 'brainfuckinterperter',
         name: 'BrainFuck Intepreter',
+        color1:"#c200d4",
+        color2:"#a802b8",
+        color3:"#56005e",
+
         blocks: [
             {
                 blockType: Scratch.BlockType.LABEL,
@@ -21,7 +90,7 @@ class BrainFuckInterperter{
           {
             opcode: 'createContext',
             blockType: Scratch.BlockType.COMMAND,
-            text: 'Create context named: [name]',
+            text: 'Create/Reset context named: [name]',
             arguments: {
                 name: {
                   type: Scratch.ArgumentType.STRING,
@@ -64,7 +133,7 @@ class BrainFuckInterperter{
           {
             opcode: 'runCodeReporter',
             blockType: Scratch.BlockType.REPORTER,
-            text: 'In context: [name] run [code]',
+            text: 'In context: [name] run: [code] with input byte: [input]',
             arguments: {
                 name: {
                   type: Scratch.ArgumentType.STRING,
@@ -73,13 +142,88 @@ class BrainFuckInterperter{
                 code: {
                     type: Scratch.ArgumentType.STRING,
                     defaultValue:">>+"
+                },
+                input: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue:"h"
                 }
               }
           },
-        ]
+          {
+            opcode: 'runCodeCommand',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'In context: [name] run: [code] with input byte: [input]',
+            arguments: {
+                name: {
+                  type: Scratch.ArgumentType.STRING,
+                  defaultValue:"main"
+                },
+                code: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue:">>+"
+                },
+                input: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue:"h"
+                }
+              }
+          },
+
+          {
+            opcode: 'allowErrors',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Set allow errors: [value]',
+            arguments: {
+                value: {
+                    type: Scratch.ArgumentType.STRING,
+                    menu: 'BOOL_MENU'
+                  },
+              }
+          },
+
+        ],
+        menus: {
+            BOOL_MENU: {
+              acceptReporters: true,
+              items: [
+                {
+                  text: 'true',
+                  value: true
+                },
+                {
+                  text: 'false',
+                  value: false
+                }
+              ]
+            }
+        }
       };
     }
   
+    allowErrors(args){
+        allowErrors = args.value
+    }
+
+    runCodeReporter(args){
+        let result = bf(args.code,this.contexts[args.name],args.input)
+        if(result[0].startsWith("Error:")&&this.allowErrors == false){
+            console.warn(result)
+        }else{
+            this.contexts[args.name] = result[1]
+        }
+        return result[0]
+    }
+    runCodeCommand(args){
+        let result = bf(args.code,this.contexts[args.name],args.input)
+        if(result[0].startsWith("Error:")&&this.allowErrors == false){
+            return result[0]
+        }else{
+            this.contexts[args.name] = result[1]
+        }
+        // return result[0]
+    }
+    
+
     createContext(args) {
         this.contexts[args.name] = {
             memoryPointer:0,
@@ -97,68 +241,7 @@ class BrainFuckInterperter{
         this.contexts = {}
     }
 
-    bf(code, data, input) {
-        let result = ""
-        let memory = data.memory
-        let loc = data.memoryPointer
-        let throwErr = (err, i) => {
-          return ["Error: " + err + ` (Char:${i}, MemoryLocation:${loc})`, {
-            memoryPointer: loc,
-            memory: memory
-          }]
-        }
-        for (let i = 0; i < code.length; i++) {
-          switch (code[i]) {
-            case ("+"):
-              memory[loc]++
-              break;
-            case ("-"):
-              memory[loc]--
-              break;
-            case (">"):
-              loc++
-              if (memory[loc] == undefined) memory.push(0)
-              break;
-            case ("<"):
-              loc--
-              if (memory[loc] == undefined) {
-                loc++
-                return throwErr(`Memory location can't go negative!`, i)
-              }
-              break;
-            case ("["):
-              if (memory[loc] == 0) {
-                while (true) {
-                  if (code.length < i || code[i] == "]") break
-                  i++
-                }
-              }
-              break;
-            case ("]"):
-              if (memory[loc] != 0) {
-                while (true) {
-                  if (code.length < i || code[i] == "[") break
-                  i--
-                }
-              }
-              break;
-            case ("."):
-              result += String.fromCharCode(memory[loc])
-              break;
-            case (" "):
-              break;
-            case (","):
-              memory[loc] = input.charCodeAt(0)
-              break;
-            default:
-              return throwErr(`Invalid command: ${code[i]}`, i)
-          }
-        }
-        return [result, {
-          memoryPointer: loc,
-          memory: memory
-        }]
-      }
+   
 
 
   }

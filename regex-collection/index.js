cef60b41048682b4d7cbcaef07eadd68e0877bad
AshimeeAlt <161080149+AshimeeAlt@users.noreply.github.com>
2024-03-22 19:37:33 -0400

    fixed lol

--- regex-collection/index.js
+++ regex-collection/index.js
@@ -1,59 +1,62 @@
-class regexCollection {
-    constructor(){
-        this.basic = [
-            ["Valid password (Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:)","/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$/"],
-            ["Email (Allows executing and testing)","/^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\.([a-z\\.]{2,6})$/"]
-        ]
+(function (Scratch) {
+  class regexCollection {
+    constructor() {
+      this.basic = [
+        [
+          'Valid password (Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:)',
+          '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$/',
+        ],
+        ['Email (Allows executing and testing)', '/^([a-z0-9_\\.-]+)@([\\da-z\\.-]+).([a-z\\.]{2,6})$/'],
+      ];
 
-        this.basicRegexConstructed = []
-        this.basic.forEach(e => {
-            this.basicRegexConstructed.push({
-                text: e[0],
-                value: e[1]
-              })
+      this.basicRegexConstructed = [];
+      this.basic.forEach((e) => {
+        this.basicRegexConstructed.push({
+          text: e[0],
+          value: e[1],
         });
+      });
     }
     getInfo() {
       return {
-        id: 'regexCollection',
+        id: 'minidoggRegexCollection',
         name: 'Regex Collection',
-        color1:"#00691c",
-        color2:"#00611a",
-        color3:"#014012",
+        color1: '#00691c',
+        color2: '#00611a',
+        color3: '#014012',
         blocks: [
           {
             blockType: Scratch.BlockType.LABEL,
-            text: "Blocks for using RegExp"
+            text: 'Blocks for using RegExp',
           },
           {
             opcode: 'exec',
             blockType: Scratch.BlockType.REPORTER,
             text: 'Execute regex: [regex] on string: [string]',
             arguments: {
-                regex: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue:"/(abc)/gi"
-                  },
-                  string: {
-                      type: Scratch.ArgumentType.STRING,
-                      defaultValue:"__abc__"
-                  },
-            }
+              regex: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '/(abc)/gi',
+              },
+              string: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '__abc__',
+              },
+            },
           },
           {
             opcode: 'test',
             blockType: Scratch.BlockType.BOOLEAN,
             text: 'Test regex: [regex] on string: [string]',
             arguments: {
-                regex: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:"/abc/gi"
-                },
-                string: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue:"__abc__"
-                },
-
+              regex: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '/abc/gi',
+              },
+              string: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '__abc__',
+              },
             },
           },
           {
@@ -61,92 +64,87 @@ class regexCollection {
             blockType: Scratch.BlockType.BOOLEAN,
             text: 'Get index [index] in [array]',
             arguments: {
-                index: {
-                  type: Scratch.ArgumentType.NUMBER,
-                  defaultValue:0
-                },
-                array: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue:`["a","b","c"]`
-                },
-
+              index: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 0,
+              },
+              array: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: `["a","b","c"]`,
+              },
             },
           },
           {
             blockType: Scratch.BlockType.LABEL,
-            text: "RegExp Utilities"
+            text: 'RegExp Utilities',
           },
           {
             opcode: 'escapeString',
             blockType: Scratch.BlockType.REPORTER,
             text: 'Escape string for regex: [string]',
             arguments: {
-                string: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'https://www.google.com'
-                }
-
+              string: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://www.google.com',
+              },
             },
           },
           {
             blockType: Scratch.BlockType.LABEL,
-            text: "Basic RegExp collection"
+            text: 'Basic RegExp collection',
           },
           {
             opcode: 'basicRegex',
             blockType: Scratch.BlockType.REPORTER,
             text: 'Get pre-built RegExp [regex]',
             arguments: {
-                regex: {
-                    type: Scratch.ArgumentType.STRING,
-                    menu: 'BASIC_REGEX'
-                }
-
+              regex: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'BASIC_REGEX',
+              },
             },
           },
           {
             blockType: Scratch.BlockType.LABEL,
-            text: "Template RegExp Builders"
+            text: 'Template RegExp Builders',
           },
           {
             blockType: Scratch.BlockType.LABEL,
-            text: "RegExp Building Blocks"
+            text: 'RegExp Building Blocks',
           },
           {
             opcode: 'join',
             blockType: Scratch.BlockType.REPORTER,
             text: 'Join [a] [b] [c] [e] [f] [g]',
             arguments: {
-                a: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue:"/"
-                },
-                b: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:""
-                },
-                c: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:""
-                },
-                d: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:""
-                },
-                e: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:""
-                },
-                f: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:"/"
-                },
-                g: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue:"gi"
-                },
-                
-
+              a: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '/',
+              },
+              b: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '',
+              },
+              c: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '',
+              },
+              d: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '',
+              },
+              e: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '',
+              },
+              f: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '/',
+              },
+              g: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'gi',
+              },
             },
           },
           {
@@ -154,84 +152,88 @@ class regexCollection {
             blockType: Scratch.BlockType.REPORTER,
             text: '[base]',
             arguments: {
-                base: {
-                    type: Scratch.ArgumentType.STRING,
-                    menu: 'BASE'
-                }
-
+              base: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'BASE',
+              },
             },
           },
-          
         ],
         menus: {
-            BASIC_REGEX: {
-              acceptReporters: true,
-              items: this.basicRegexConstructed
-            },
-            BASE:{
-              acceptReporters:true,
-              items:[
-                {
-                  text:"Global flag",
-                  value:"g"
-                },
-                {
-                  text:"Insensitive flag",
-                  value:"i"
-                },
-              ]
-            }
-          }
+          BASIC_REGEX: {
+            acceptReporters: true,
+            items: this.basicRegexConstructed,
+          },
+          BASE: {
+            acceptReporters: true,
+            items: [
+              {
+                text: 'Global flag',
+                value: 'g',
+              },
+              {
+                text: 'Insensitive flag',
+                value: 'i',
+              },
+            ],
+          },
+        },
       };
     }
-  
+
     exec(args) {
+      args.regex = Scratch.Cast.toString(args.regex);
+      args.string = Scratch.Cast.toString(args.string);
       var parts = /\/(.*)\/(.*)/.exec(args.regex);
       var restoredRegex = new RegExp(parts[1], parts[2]);
 
-      let result = restoredRegex.exec(args.string)
-      return typeof(result)=="object"?JSON.stringify(result):[result]
+      let result = restoredRegex.exec(args.string);
+      return typeof result == 'object' ? JSON.stringify(result) : [result];
     }
     test(args) {
-        var parts = /\/(.*)\/(.*)/.exec(args.regex);
-        var restoredRegex = new RegExp(parts[1], parts[2]);
-        
+      args.regex = Scratch.Cast.toString(args.regex);
+      args.string = Scratch.Cast.toString(args.string);
+      var parts = /\/(.*)\/(.*)/.exec(args.regex);
+      var restoredRegex = new RegExp(parts[1], parts[2]);
 
-        let result = restoredRegex.test(args.string)
-        return result
-      }
+      let result = restoredRegex.test(args.string);
+      return result;
+    }
 
-      getCaptureGroup(args){
-        return args.array[0]=="["?JSON.parse(args.array)[args.index]:"Invalid array!"
-      }
+    getCaptureGroup(args) {
+      args.array = Scratch.Cast.toString(args.array);
+      args.index = Scratch.Cast.toNumber(args.index);
+      return args.array[0] == '[' ? JSON.parse(args.array)?.[args.index] : 'Invalid array!';
+    }
 
-      basicRegex(args){
-        return args.regex
-      }
-      readBase(args){
-        return args.base
-      }
+    basicRegex(args) {
+      args.regex = Scratch.Cast.toString(args.regex);
+      return args.regex;
+    }
+    readBase(args) {
+      args.base = Scratch.Cast.toString(args.base);
+      return args.base;
+    }
 
-      escapeString(args){
-        let chars = args.string.split("")
-        let result = ""
-        chars.forEach((e)=>{
-          result+=(".^$*+?()[{\|".includes(e.toLowerCase()))?("\\"+e):e
-        })
-        
-        return result
-      }
-      
+    escapeString(args) {
+      args.string = Scratch.Cast.toString(args.string);
+      let chars = args.string.split('');
+      let result = '';
+      chars.forEach((e) => {
+        result += '.^$*+?()[{|'.includes(e.toLowerCase()) ? '\\' + e : e;
+      });
 
-      join(args){
-        let result = ""
-        Object.keys(args).forEach((e)=>{
-          result += (args[e]!=undefined?args[e]:"")
-        })
-        return result
-      }
+      return result;
+    }
 
-
+    join(args) {
+      let result = '';
+      Object.keys(args).forEach((e) => {
+        result += args[e] != undefined ? args[e] : '';
+      });
+      return result;
+    }
   }
-  
+
   Scratch.extensions.register(new regexCollection());
+})(Scratch);
